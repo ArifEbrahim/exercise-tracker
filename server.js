29abffda2e0dfd959f21d5d2368602167e077564
Arif Ebrahim <74261924+ArifEbrahim@users.noreply.github.com>
2022-03-30 21:28:29 +0100

    feature: exercises can be saved

--- server.js
+++ server.js
@@ -16,26 +16,70 @@ mongoose.connect(process.env.MONGO_URI, {
   useUnifiedTopology: true,
 });
 
-const Schema = new mongoose.Schema({
+const UserSchema = new mongoose.Schema({
   username: String,
 });
 
-const User = mongoose.model("User", Schema);
+const ExerciseSchema = new mongoose.Schema({
+  userId: String,
+  description: String,
+  duration: Number,
+  date: Date,
+});
+
+const User = mongoose.model("User", UserSchema);
+const Exercise = mongoose.model('Exercise', ExerciseSchema);
 
 app.post("/api/users", async (req, res) => {
   const username = req.body.username;
   const user = new User({ username });
   await user.save((err, data) => {
-    res.json({
-      username: data.username,
-      _id: data.id,
-    });
+    if (err || !data) {
+      res.send("Error: user not saved");
+    } else {
+      res.json({
+        username: data.username,
+        _id: data.id,
+      });
+    }
   });
 });
 
 app.get("/api/users", async (req, res) => {
   const result = await User.find({});
   res.json(result);
+});
+
+app.post("/api/users/:_id/exercises", (req, res) => {
+  const _id = req.params._id;
+  User.findById({_id}, (err, userData) => {
+    if (err || !userData) {
+      res.send('Error: user not found')
+    } else {
+      const { description, duration, date } = req.body;
+      const exercise = new Exercise({
+        userId: _id,
+        description,
+        duration,
+        date: new Date(date)
+      })
+      exercise.save((err, data) => {
+        if(err || !data) {
+          res.send('Error: exercise not saved')
+        } else {
+          const { description, duration, date, userId } = data;
+          const { username } = userData;
+          res.json({
+            username,
+            description,
+            duration,
+            date: date.toDateString(),
+            _id: userId
+          })
+        }
+      })
+    }
+  })
 });
 
 app.listen(3000);
